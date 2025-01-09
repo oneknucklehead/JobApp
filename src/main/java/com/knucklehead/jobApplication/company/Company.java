0e54e4198fb34fb6a5aa2ff3caa724505a5be16f
Zoheb <zohebcool1542@gmail.com>
2025-01-10 02:21:30 +0530

    reviews implemented

--- src/main/java/com/knucklehead/jobApplication/company/Company.java
+++ src/main/java/com/knucklehead/jobApplication/company/Company.java
@@ -2,6 +2,7 @@ package com.knucklehead.jobApplication.company;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.knucklehead.jobApplication.job.Job;
+import com.knucklehead.jobApplication.review.Review;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -17,9 +18,19 @@ public class Company {
     @JsonIgnore
     @OneToMany(mappedBy = "company")
     private List<Job> jobs;
-//    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "company")
+    private List<Review> reviews;
 
     public Company() {
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
     }
 
     public Long getId() {
